3bdb182f4062bc5c1499fcd075d39ab499437cac
BethG <bgarrisn@temple.edu>
2020-09-17 17:03:41 -0400

    updated 06_03 :-)

--- 06_functions/06_03_three_functions.py
+++ 06_functions/06_03_three_functions.py
@@ -3,32 +3,38 @@ Write a program with 3 functions. Each function must call
 at least one other function and use the return value to do something.
 
 '''
+
+def base(x):
+    if int(x)> 2:
+        x=x*2
+        return x
+    return 0
+
+
 def fun1(x):
-    if fun2(y)==1:
+    if base(x)==1:
         return True
-    else:
-        return False
+    return False
 
 def fun2(y):
     if fun1(x)==1:
         return True
-    else:
-        return False
+    return False
 
 def fun3(z):
     if fun2(y) and fun1(x)==1:
         return True
-    else:
-        return False
+    return False
 
 x=input("give me a number between 1 and 10: ")
 y=input("give me a number between 1 and 10: ")
 z=input("give me a number between 1 and 10: ")
 
-answer1=fun1(int(x))
-answer2=fun2(int(y))
-answer3=fun3(int(z))
 
-print(answer1)
-print(answer2)
-print(answer3)
+
+print(fun1(x))
+print(fun2(y))
+print(fun3(z))
+
+
+
